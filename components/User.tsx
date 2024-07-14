794ed090598ed3c2838c76ba3f8ba3da93fd3f8c
Akshay Nair <akshayakn6@gmail.com>
2024-07-13 20:44:08 -0400

    test: add console tests for User component

--- components/User.tsx
+++ components/User.tsx
@@ -9,13 +9,24 @@ import { getUser, setUser } from "@/utils/dexieUtils";
 const User: React.FC = () => {
   const { data: session, status } = useSession();
   const userData = useGetDexieDB<UserData>("users", "1");
-
-  useEffect(() => {
+  const [name, setName] = useState<string|undefined>("");
+  function run() {
+    console.log('running');
+    
     getUser("1").then((data) => {
-      console.log("loaded from server", data);
+      console.log("loaded from indexeddb", data);
+      setName(data?.name)
+    });
+  }
+  useEffect(() => {
+    console.log("checking indexed db");
+    
+    getUser("1").then((data) => {
+      console.log("loaded from indexeddb", data);
     });
     if (session?.user) {
-      setUser({
+    console.log("checking indexed db");
+    setUser({
         id: "1", //Removed session.user.id || '' (for fast dexie query)
         name: session.user?.name || "",
         email: session.user?.email || "",
@@ -37,7 +48,9 @@ const User: React.FC = () => {
       //   updateName(session);
     }
   }, [session]);
-  return <div className="user">User Avatar: {userData?.email}</div>;
+  return <div className="user">User Avatar: {userData?.email}
+  <button onClick={() => run()}>run {name}</button>
+  </div>;
 };
 
 export default User;
